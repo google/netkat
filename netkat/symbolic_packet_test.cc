e5e70966d739c1765e25ba19bb173429dd22464f
Jonathan DiLorenzo <dilo@google.com>
2025-04-14 13:59:09 -0700

    [Netkat] Add note to use manager pretty printers.
    PiperOrigin-RevId: 747557692

--- netkat/symbolic_packet_test.cc
+++ netkat/symbolic_packet_test.cc
@@ -56,7 +56,7 @@ using ::testing::StartsWith;
 class CheckSymbolicPacketManagerInvariantsOnTearDown
     : public testing::Environment {
  public:
-  ~CheckSymbolicPacketManagerInvariantsOnTearDown() override {}
+  ~CheckSymbolicPacketManagerInvariantsOnTearDown() override = default;
   void SetUp() override {}
   void TearDown() override { ASSERT_OK(Manager().CheckInternalInvariants()); }
 };
