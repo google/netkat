cb114b656ded3b9a18908692f58e58473d9c500b
Jonathan DiLorenzo <dilo@google.com>
2026-03-26 14:36:05 -0700

    [NetKAT] Move Packet definition and helper functions to a dedicated file.
    PiperOrigin-RevId: 890052989

--- netkat/counter_example_test.cc
+++ netkat/counter_example_test.cc
@@ -6,8 +6,8 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "gutil/status_matchers.h"
-#include "netkat/evaluator.h"
 #include "netkat/frontend.h"
+#include "netkat/packet.h"
 #include "netkat/packet_transformer.h"
 
 namespace netkat {
@@ -15,6 +15,8 @@ namespace {
 
 using ::gutil::IsOkAndHolds;
 using ::gutil::StatusIs;
+using ::testing::HasSubstr;
+using ::testing::StrEq;
 
 TEST(CounterExampleTest,
      CreateEquivalenceCounterExampleReturnsErrorIfPoliciesAreTheSame) {
@@ -36,11 +38,21 @@ TEST(CounterExampleTest, CreateEquivalenceCounterExampleReturnsCounterExample) {
               IsOkAndHolds(Packet{}));
   EXPECT_THAT(counter_example.GetInputPacketInRightButNotLeft(),
               StatusIs(absl::StatusCode::kNotFound));
+
+  // The explanation should include the input packet and output packet, which in
+  // this case are the same.
+  EXPECT_THAT(counter_example.Explain(), HasSubstr("{}"));
+
+  // The explanation should indicate that the output packet produced is from the
+  // left policy, but not the right policy.
+  EXPECT_THAT(counter_example.Explain(),
+              HasSubstr("left policy, but not on right policy"));
 }
 
 TEST(SuccessOrCounterExampleTest, DefaultConstructorIsSuccess) {
   SuccessOrCounterExample success_or_counter_example;
   EXPECT_TRUE(success_or_counter_example.IsSuccess());
+  EXPECT_THAT(success_or_counter_example.Explain(), StrEq("Success"));
 }
 
 TEST(SuccessOrCounterExampleTest, ConstructorWithCounterExampleIsNotSuccess) {
@@ -53,6 +65,16 @@ TEST(SuccessOrCounterExampleTest, ConstructorWithCounterExampleIsNotSuccess) {
   SuccessOrCounterExample success_or_counter_example(
       std::move(counter_example));
   EXPECT_FALSE(success_or_counter_example.IsSuccess());
+  EXPECT_THAT(success_or_counter_example.Explain(), Not(StrEq("Success")));
+
+  // The explanation should include the input packet and output packet, which in
+  // this case are the same.
+  EXPECT_THAT(success_or_counter_example.Explain(), HasSubstr("{}"));
+
+  // The explanation should indicate that the output packet produced is from the
+  // right policy, but not the left policy.
+  EXPECT_THAT(success_or_counter_example.Explain(),
+              HasSubstr("right policy, but not on left policy"));
 }
 
 TEST(SuccessOrCounterExampleTest,
@@ -71,6 +93,13 @@ TEST(SuccessOrCounterExampleTest,
   EXPECT_THAT(success_or_counter_example.GetCounterExampleOrDie()
                   .GetInputPacketInLeftButNotRight(),
               IsOkAndHolds(Packet{}));
+
+  // The explanation should include the input packet and output packet. Since
+  // the order of fields in the packet map is unspecified, we check for the
+  // presence of the fields in the explanation rather than the full packet.
+  EXPECT_THAT(success_or_counter_example.Explain(),
+              testing::AllOf(HasSubstr("{}"), HasSubstr("switch=1"),
+                             HasSubstr("port=1")));
 }
 
 }  // namespace
